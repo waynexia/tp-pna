eddc74688f0bcd28777a238e8129db21756fbec0
Ruihang Xia <waynestxia@gmail.com>
2019-09-16 00:19:41 +0800

    config clap

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,14 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = clap::load_yaml!("cli.yml");
+    let m = App::from_yaml(yaml)
+        .name(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .get_matches();
+
+    println!("{:?}", m)
 }
