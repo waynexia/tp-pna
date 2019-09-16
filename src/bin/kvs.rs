c7252d91da9bd6ac18ac4319d161ac10569f94a8
Ruihang Xia <waynestxia@gmail.com>
2019-09-16 09:43:02 +0800

    cli tests passed

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,14 +1,35 @@
 extern crate clap;
 use clap::App;
+use std::process::exit;
+// use kvs::KvStore;
 
 fn main() {
+    /* load clap config from yaml file */
     let yaml = clap::load_yaml!("cli.yml");
-    let m = App::from_yaml(yaml)
+    let matches = App::from_yaml(yaml)
         .name(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .get_matches();
 
-    println!("{:?}", m)
+    // let mut kvstore = KvStore::new();
+
+    match matches.subcommand() {
+        ("get", Some(sub_m)) => {
+            sub_m.value_of("key").unwrap();
+            eprintln!("unimplemented!");
+            exit(1);
+        }
+        ("set", Some(sub_m)) => {
+            eprintln!("unimplemented!");
+            exit(1);
+        }
+        ("rm", Some(sub_m)) => {
+            eprintln!("unimplemented!");
+            exit(1);
+        }
+
+        _ => unreachable!(),
+    }
 }
