866f0e2eb103e376738b17c878256d7bcecab699
Ruihang Xia <waynestxia@gmail.com>
2019-09-21 21:55:51 +0800

    connection between client and server established

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,8 +1,25 @@
 extern crate clap;
+use serde::{Deserialize, Serialize};
 use clap::App;
 use kvs::{KvsError, KvStore, Result};
 use std::path::Path;
 use std::process::exit;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum OpType {
+    Set,
+    Get,
+    Remove,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Command {
+    op: OpType,
+    key: String,
+    value: Option<String>,
+}
 
 fn main() -> Result<()> {
     /* load clap config from yaml file */
@@ -16,32 +33,62 @@ fn main() -> Result<()> {
 
     let mut kvstore = KvStore::open(&Path::new("./"))?;
 
+
+    let mut command = Command{
+        op: OpType::Get,
+        key: String::new(),
+        value: None,
+    };
+    let mut addr = String::new();
+
     match matches.subcommand() {
         ("get", Some(sub_m)) => {
-            match kvstore.get(sub_m.value_of("key").unwrap().to_owned())? {
-                Some(value) => println!("{}", value),
-                None => println!("Key not found"),
-            };
+            // match kvstore.get(sub_m.value_of("key").unwrap().to_owned())? {
+            //     Some(value) => println!("{}", value),
+            //     None => println!("Key not found"),
+            // };
+            command.key = sub_m.value_of("key").unwrap().to_owned();
+            addr = sub_m.value_of("addr").unwrap_or("127.0.0.1:4000").to_owned();
         }
         ("set", Some(sub_m)) => {
-            kvstore.set(
-                sub_m.value_of("key").unwrap().to_owned(),
-                sub_m.value_of("value").unwrap().to_owned(),
-            )?;
+            // kvstore.set(
+            //     sub_m.value_of("key").unwrap().to_owned(),
+            //     sub_m.value_of("value").unwrap().to_owned(),
+            // )?;
+
+            command.op = OpType::Set;
+            command.key = sub_m.value_of("key").unwrap().to_owned();
+            command.value = Some(sub_m.value_of("value").unwrap().to_owned());
+            addr = sub_m.value_of("addr").unwrap_or("127.0.0.1:4000").to_owned();
         }
         ("rm", Some(sub_m)) => {
-            match kvstore.remove(sub_m.value_of("key").unwrap().to_owned()) {
-                Err(KvsError::KeyNotFound) => {
-                    println!("Key not found");
-                    exit(1);
-                }
-                Ok(()) => {},
-                Err(e) => return Err(e),
-            }
+            // match kvstore.remove(sub_m.value_of("key").unwrap().to_owned()) {
+            //     Err(KvsError::KeyNotFound) => {
+            //         println!("Key not found");
+            //         exit(1);
+            //     }
+            //     Ok(()) => {},
+            //     Err(e) => return Err(e),
+            // }
+            command.op = OpType::Remove;
+            command.key = sub_m.value_of("key").unwrap().to_owned();
+            addr = sub_m.value_of("addr").unwrap_or("127.0.0.1:4000").to_owned();
         }
 
         _ => unreachable!(),
     }
+
+
+    let mut stream = TcpStream::connect(&addr)?;
+    let content = serde_json::to_vec(&command)?;
+    let len = serde_json::to_vec(&content.len())?;
+    stream.write(&len)?;
+    stream.write(&content)?;
+    println!("sended!");
+    let mut rcv_message = vec![];
+    stream.read_to_end(&mut rcv_message)?;
+    let de : Command = serde_json::from_slice(&rcv_message)?;
+    println!("{:?}",de);
 
     Ok(())
 }
--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,16 +1,62 @@
 extern crate clap;
 use clap::App;
 use kvs::Result;
+use serde::{Deserialize, Serialize};
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+extern crate slog_async;
+
+use slog::Drain;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum OpType {
+    Set,
+    Get,
+    Remove,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Command {
+    op: OpType,
+    key: String,
+    value: Option<String>,
+}
 
 fn main() -> Result<()> {
     /* load clap config from yaml file */
     let yaml = clap::load_yaml!("kvs-server-clap.yml");
-    let matches = App::from_yaml(yaml)
+    let _matches = App::from_yaml(yaml)
         .name(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .get_matches();
+
+    let decorator = slog_term::PlainDecorator::new(std::io::stdout());
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let log = slog::Logger::root(drain, o!());
+
+    info!(log, "start");
+    // debug!(log, "debug");
+    // warn!(log, "warn");
+    let listener = TcpListener::bind("127.0.0.1:4000")?;
+    for stream in listener.incoming() {
+        let mut s = stream?;
+        let mut buf = vec![0,8];
+        println!("received!");
+        s.read_exact(&mut buf)?;
+        let len :usize = serde_json::from_slice(&buf)?;
+        buf.resize(len,0);
+        s.read_exact(&mut buf);
+        let something : Command = serde_json::from_slice(&buf)?;
+        info!(log,"{:?}",something);
+        s.write(&buf)?;
+    }
 
     Ok(())
 }
